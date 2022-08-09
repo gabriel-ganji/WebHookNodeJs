852ec61ce6e42f003cb1fc029003e722aea9a006
marycosta <mary.costa201025@gmail.com>
2022-08-09 16:00:47 -0300

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,18 @@
-const { default: mongoose } = require("mongoose");
-import mongoDBcredentials from '../credentials/credentials'
-mongoose.connect(
-    "mongodb+srv://interships:y60TGJW5Q8cEJ3Tx@evermartmongodb.dbkte1c.mongodb.net/?retryWrites=true&w=majority"
-);
+// Arquivo que contém a conexão com o banco de dados.
+
+const { default: mongoose, connect, connection } = require("mongoose");
+
+const {mongoDBcredentials, mongoDBconnection} =  require("../credentials/credentials");
+
+try{
+    mongoose.connect(
+        `mongodb+srv://${mongoDBcredentials["username"]}:${mongoDBcredentials["password"]}@${mongoDBconnection["cluster"]}.mongodb.net/?retryWrites=true&w=majority `
+        );
+} catch (error) {
+    res.status(500).json({ error: error });
+}
+const dbConnection = mongoose.connection;
+dbConnection.on("error", console.error.bind(console,"connection error:"));
+dbConnection.once("open", function(){
+    console.log("Connected sucessfully!");
+})
