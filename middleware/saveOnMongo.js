4f1bf57d75b9ec75065948bd87c1a83e8ac80d0e
Gabrielganji <gabrielganji01011010@gmail.com>
2022-08-18 16:01:38 -0300

    trying to delete data after expiration

--- middleware/saveOnMongo.js
+++ middleware/saveOnMongo.js
@@ -6,12 +6,12 @@ const save = function (data) {
     const token = data.header.uuid;
     const header = data.header;
     const body = data.body;
-    const date = new Date();
-    const created_at = String(date);
+    const created_at = new Date();
 
     const acess = { token, header, body, created_at };
     
     try {
+        
         Acess.create(acess);
         return { message: "Dados armazenados com sucesso no MongoDBAtlas"}
     } catch (error) {
