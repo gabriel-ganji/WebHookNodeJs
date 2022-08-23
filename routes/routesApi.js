e0c6a7290f5bca81badbcec064fa00a48a3fd2dd
Fernanda Bonfim <82246157+fernandabonfimm@users.noreply.github.com>
2022-08-23 14:20:50 -0300

    arrumando https

--- routes/routesApi.js
+++ routes/routesApi.js
@@ -39,7 +39,7 @@ router.get("/geturluuid", async (req, res) => {
         message: "Algo deu errado, tente novamente.",
       });
   } else {
-    res.status(200).json(`https://create-react-app-fernandabonfimm.vercel.app/${uuid}`);
+    res.status(200).json(`https://everhooks-web.vercel.app/${uuid}`);
   }
 });
 
