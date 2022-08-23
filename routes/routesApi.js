418249fc441480367709d7e8142b2968b04145d1
Fernanda Bonfim <82246157+fernandabonfimm@users.noreply.github.com>
2022-08-23 14:01:54 -0300

    arrumando https

--- routes/routesApi.js
+++ routes/routesApi.js
@@ -12,81 +12,98 @@ const findSubData = express("../middleware/findSubData");
 router.use(express.json());
 
 //Rota para gerar uuid e armazenar uuid no mongo
- router.get("/", async (req, res) => {
+router.get("/", async (req, res) => {
+  const urluuid = await generateAndSaveUUID(req);
 
-     const urluuid = await generateAndSaveUUID(req);
-
-     if (urluuid.length !== 36 || urluuid == undefined) {
-   
-         res.status(400).json({ Error: 400, Type: "Bad Request", message: "Algo deu errado, tente novamente." });
-   
-     } else {
-  
-         res.status(200).json(urluuid);
-  
-     }
+  if (urluuid.length !== 36 || urluuid == undefined) {
+    res
+      .status(400)
+      .json({
+        Error: 400,
+        Type: "Bad Request",
+        message: "Algo deu errado, tente novamente.",
+      });
+  } else {
+    res.status(200).json(urluuid);
+  }
 });
-router.get("/geturluuid", async(req, res) => {
-    const uuid = generateAndSaveUUID(req);
-    // console.log(uuid);
-    if (uuid.length !== 36 || uuid == undefined) {
-        res.status(400).json({ Error: 400, Type: "Bad Request", message: "Algo deu errado, tente novamente." });
-    } else {
-        res.status(200).json( `create-react-app-xi-three-32.vercel.app/${uuid}` );
-    }
+router.get("/geturluuid", async (req, res) => {
+  const uuid = generateAndSaveUUID(req);
+  // console.log(uuid);
+  if (uuid.length !== 36 || uuid == undefined) {
+    res
+      .status(400)
+      .json({
+        Error: 400,
+        Type: "Bad Request",
+        message: "Algo deu errado, tente novamente.",
+      });
+  } else {
+    res.status(200).json(`create-react-app-xi-three-32.vercel.app/${uuid}`);
+  }
 });
 
 //Emissão de dados do database para o front
 router.get("/:uuid", async (req, res) => {
+  const data = await getData(req.params.uuid);
 
-    const data = await getData(req.params.uuid);
-
-    if (req.params.uuid.length !== 36) {
-    
-        res.status(400).json({ Error: 400, Type: "Bad Request", message: "Algo deu errado, tente novamente." });
-
+  if (req.params.uuid.length !== 36) {
+    res
+      .status(400)
+      .json({
+        Error: 400,
+        Type: "Bad Request",
+        message: "Algo deu errado, tente novamente.",
+      });
+  } else {
+    if (data === []) {
+      res
+        .status(400)
+        .json({
+          Error: 400,
+          Type: "Bad Request",
+          message: "Algo deu errado, tente novamente.",
+        });
     } else {
-        
-        if (data === []) {
-            res.status(400).json({ Error: 400, Type: "Bad Request", message: "Algo deu errado, tente novamente." });
-        } else {
-            res.status(200).json(data);
-        }
+      res.status(200).json(data);
     }
+  }
 });
 
 //Rota de gravação de dados no data base
 router.post("/:uuid", async (req, res) => {
+  console.log(req);
 
-    console.log(req);
+  if (req.params.uuid.length !== 36) {
+    res
+      .status(400)
+      .json({
+        Error: 400,
+        Type: "Bad Request",
+        message: "Algo deu errado, tente novamente.",
+      });
+  } else {
+    const data = await getData(req.params.uuid);
 
-    if (req.params.uuid.length !== 36) {
-
-        res.status(400).json({ Error: 400, Type: "Bad Request", message: "Algo deu errado, tente novamente." });
-    
+    if (data === []) {
+      res
+        .status(400)
+        .json({
+          Error: 400,
+          Type: "Bad Request",
+          message: "O token de sua urluuid não é válido",
+        });
     } else {
-        
-        const data = await getData(req.params.uuid);
-
-        if (data === []) {
-        
-            res.status(400).json({ Error: 400, Type: "Bad Request", message: "O token de sua urluuid não é válido" });
-
-        } else {
-
-            const handle = handleData(req.params.uuid, req);
-            res.status(200).json(handle);
-       
-        }
+      const handle = handleData(req.params.uuid, req);
+      res.status(200).json(handle);
     }
-
+  }
 });
 
 router.delete("/:uuid", async (req, res) => {
-    console.log(req.params.uuid);
-    await Acess.deleteMany({ token: req.params.uuid });
-    res.status(200).json({ message: "ok" });
-
+  console.log(req.params.uuid);
+  await Acess.deleteMany({ token: req.params.uuid });
+  res.status(200).json({ message: "ok" });
 });
 
 module.exports = router;
